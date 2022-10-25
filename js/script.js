ebd19ecabcaca3649eae0718fa514622f7034022
Carlos Xavier <carlos.lipe559@hotmail.com>
2022-10-25 15:23:02 -0300

    add animations

--- js/script.js
+++ js/script.js
@@ -62,3 +62,24 @@ function faq() {
 
 }
 faq();
+
+const animations = document.querySelectorAll("[data-animation]");
+const animationClass = "animate";
+
+function animation_scroll() {
+    const area_window = window.innerHeight * 0.21 * 3.9;
+
+    animations.forEach((element) => {
+        let posicaoAtual = element.getBoundingClientRect().top;
+
+        if (area_window > posicaoAtual) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    });
+}
+
+if (animations.length) {
+    window.addEventListener("scroll", animation_scroll);
+}
